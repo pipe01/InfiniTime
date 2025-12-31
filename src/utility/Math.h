7e52d6ffb469e15d5d0574feaaaba410f52d4988
Felipe Martínez <felipe@pipe01.net>
2025-12-31 18:40:34 +0100

    Update src/utility/Math.h
    Co-authored-by: mark9064 <30447455+mark9064@users.noreply.github.com>

--- src/utility/Math.h
+++ src/utility/Math.h
@@ -30,7 +30,7 @@ namespace Pinetime {
       return res;
     }
 
-    constexpr int compileTimeAtoi(const char* str) {
+    constexpr int CompileTimeAtoi(const char* str) {
       int result = 0;
       while (*str >= '0' && *str <= '9') {
         result = result * 10 + *str - '0';
